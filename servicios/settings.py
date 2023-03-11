529c74048a6ef7e88e155e3cfe6891c1f2b36237
Marlon Polanco <mpolanco@aeroman.com.sv>
2023-03-11 17:18:28 -0600

    add link app web

--- servicios/settings.py
+++ servicios/settings.py
@@ -61,6 +61,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "http://127.0.0.1:800",
     "https://web-development-fcc7.up.railway.app",
+    "https://cafemarlon.vercel.app",
 ]
 
 ROOT_URLCONF = 'servicios.urls'
